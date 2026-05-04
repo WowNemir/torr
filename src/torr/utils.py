4a0a14f0881621396c6676a11d4949790f70025b
anemirov <sasha-nemirov@yandex.ru>
2026-05-05 01:05:15 +0400

    Change default client prefix

--- src/torr/utils.py
+++ src/torr/utils.py
@@ -46,7 +46,7 @@ def generate_peer_id():
     except:  # noqa
         version = "0000"
     id_suffix = "".join([random.choice(string.ascii_letters) for _ in range(12)])
-    peer_id = f"-GF{version}-{id_suffix}"
+    peer_id = f"-Tr{version}-{id_suffix}"
     assert len(peer_id) == 20
 
     return peer_id.encode()
