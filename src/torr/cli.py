7264b755515b7c1e9c8923d3dc54cefac140aca0
anemirov <sasha-nemirov@yandex.ru>
2026-05-08 19:09:08 +0400

    Minor change: remove redundant docs and comments

--- src/torr/cli.py
+++ src/torr/cli.py
@@ -4,25 +4,19 @@ from torr.client import TorrentClient
 
 
 def main():
-    """
-    Script for downloading torrent files
-    """
-
-    parser = ArgumentParser(main.__doc__)
+    parser = ArgumentParser(__package__)
     parser.add_argument("--torrent", type=str, help="Path of the Torrent file", required=True)
     parser.add_argument("--output-directory", default=".", type=str, help="Path to the output directory")
     parser.add_argument("--use-progress-bar", action="store_true", default=False, help="should show progress bar")
     parser.add_argument("--max-peers", type=int, default=12, help="Max connected peers")
     args = parser.parse_args()
 
-    # Create client from the BitTorrent Meta File
     torrent_client = TorrentClient(
         torrent=args.torrent,
         max_peers=args.max_peers,
         output_dir=args.output_directory,
     )
 
-    # Start downloading the file
     torrent_client.start()
 
 
